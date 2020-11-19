671a6796f968617191f11b528fa18e373b5b164e
jurgen1c <jurgen1c@gmail.com>
2020-11-19 11:10:10 -0600

    Remove bootstrap & add own sass

--- Assets/Javascript/app.js
+++ Assets/Javascript/app.js
@@ -1,8 +1,12 @@
 
 anime({
-  targets: 'h1',
-  translateX: 250,
-  rotate: '1turn',
-  backgroundColor: '#FFF',
-  duration: 5000,
+  targets: '#intro-hero polygon',
+  points: [
+    {value:'153 259, 351 150, 450 252, 600 206, 654 300, 654 432, 441 464, 201 433'},
+    {value:'435 350, 264 153, 451 187, 662 136, 761 275, 574 255, 623 438, 133 462'}
+  ],
+  easing: 'easeOutQuad',
+  duration: 2500,
+  loop: true,
+  direction: 'alternate'
 });
