9688a493100334273d48002603e504444ba59998
jurgen1c <jurgen1c@gmail.com>
2020-11-20 13:59:09 -0600

    Add custom modal, slides

--- Assets/Javascript/app.js
+++ Assets/Javascript/app.js
@@ -1,12 +1,132 @@
 
-anime({
-  targets: '#intro-hero polygon',
-  points: [
-    {value:'153 259, 351 150, 450 252, 600 206, 654 300, 654 432, 441 464, 201 433'},
-    {value:'435 350, 264 153, 451 187, 662 136, 761 275, 574 255, 623 438, 133 462'}
-  ],
-  easing: 'easeOutQuad',
-  duration: 2500,
-  loop: true,
-  direction: 'alternate'
+var tl = anime.timeline({
+  easing: 'easeOutExpo',
+  duration: 2500
 });
+
+// Add children
+tl
+.add({
+  targets: '.text',
+  translateY: ['100%', '0%']
+})
+.add({
+  targets: '.slider',
+  translateY: ['100%', '-100%'],
+})
+.add({
+  targets: '.intro-ani',
+  translateY: ['0%', '-100%']
+}, 2700)
+.add({
+  targets: 'nav',
+  translateX: ['-100%', '0%']
+})
+.add({
+  targets: '.intro-wrap',
+  opacity: [0, 1]
+}, 5800);
+
+// T-bot modal and Slider --------------------------------------------
+
+var tmodal = document.getElementById("t-botModal");
+var tbtn = document.getElementById("t-botBtn");
+var tspan = document.getElementsByClassName("t-bot-close")[0];
+
+// Get the modal
+
+// When the user clicks on the button, open the modal
+tbtn.onclick = function() {
+  tmodal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+tspan.onclick = function() {
+  tmodal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == tmodal) {
+    tmodal.style.display = "none";
+  }
+}
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("t-botSlides");
+  var dots = document.getElementsByClassName("t-bot-dot");
+  if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+// CSM Modal & Slider ------------------------------------------
+
+var cmodal = document.getElementById("csmModal");
+var cbtn = document.getElementById("csmBtn");
+var cspan = document.getElementsByClassName("csm-close")[0];
+
+// Get the modal
+
+// When the user clicks on the button, open the modal
+cbtn.onclick = function() {
+  cmodal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+cspan.onclick = function() {
+  cmodal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == cmodal) {
+    cmodal.style.display = "none";
+  }
+}
+
+var cslideIndex = 1;
+showSlidesC(cslideIndex);
+
+function plusSlidesC(n) {
+  showSlidesC(cslideIndex += n);
+}
+
+function currentSlideC(n) {
+  showSlidesC(cslideIndex = n);
+}
+
+function showSlidesC(n) {
+  var i;
+  var slides = document.getElementsByClassName("csmSlides");
+  var dots = document.getElementsByClassName("csm-dot");
+  if (n > slides.length) {cslideIndex = 1}
+    if (n < 1) {cslideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+    }
+  slides[cslideIndex-1].style.display = "block";
+  dots[cslideIndex-1].className += " active";
+}
